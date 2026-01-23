50b499f316632cd0357a48d1af37cc55beec65a1
Nazifah <Nazifah@Nazifah>
2026-01-23 22:20:32 +0600

    Added authority page switch Done with this page!!

--- NewJavaProject/src/Login/UserSignupController.java
+++ NewJavaProject/src/Login/UserSignupController.java
@@ -2,19 +2,31 @@
 package Login;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 
 
-public class UserSignupController {
+public class UserSignupController implements Initializable {
 
   private Stage stage;
   private Scene scene;
+  
+  @FXML
+  private ComboBox<String> combobox;
+  
+  ObservableList<String> list=FXCollections.observableArrayList("Barishal", "Bogra", "Brahmanbaria", "Chandpur", "Chattogram", "Comilla", "Cox's Bazar", "Dhaka", "Dinajpur", "Faridpur", "Feni", "Gazipur", "Gopalganj", "Habiganj", "Jamalpur", "Jessore", "Jhalokathi", "Jhenaidah", "Joypurhat", "Khagrachhari", "Khulna", "Kishoreganj", "Kurigram", "Kushtia", "Lakshmipur", "Madaripur", "Magura", "Manikganj", "Meherpur", "Moulvibazar", "Munshiganj", "Mymensingh", "Naogaon", "Narail", "Narsingdi", "Natore", "Netrokona", "Nilphamari", "Noakhali", "Pabna", "Panchagarh", "Patuakhali", "Pirojpur", "Rajbari", "Rajshahi", "Rangamati", "Rangpur", "Satkhira", "Shariatpur", "Sherpur", "Sirajganj", "Sunamganj", "Sylhet", "Tangail", "Thakurgaon");
   
   public void LoginPageLink(ActionEvent event)throws IOException
   {
@@ -24,6 +36,11 @@ public class UserSignupController {
       stage.setScene(scene);
       stage.show();
       
+  }
+  
+  public void initialize(URL location, ResourceBundle resources)
+  {
+      combobox.setItems(list);
   }
        
     
