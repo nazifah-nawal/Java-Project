2106e0a68fd90ab12adaba185ce750fc3eaf1bb1
Nazifah <Nazifah@Nazifah>
2026-01-24 12:42:23 +0600

    Updated userSignup controller to store info

--- NewJavaProject/src/Login/UserSignupController.java
+++ NewJavaProject/src/Login/UserSignupController.java
@@ -14,8 +14,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
+import SignUpInfo.UserSignupInfo;
 
 
 public class UserSignupController implements Initializable {
@@ -24,9 +25,37 @@ public class UserSignupController implements Initializable {
   private Scene scene;
   
   @FXML
-  private ComboBox<String> combobox;
+  private ComboBox<String> Addressbox;
   
   ObservableList<String> list=FXCollections.observableArrayList("Barishal", "Bogra", "Brahmanbaria", "Chandpur", "Chattogram", "Comilla", "Cox's Bazar", "Dhaka", "Dinajpur", "Faridpur", "Feni", "Gazipur", "Gopalganj", "Habiganj", "Jamalpur", "Jessore", "Jhalokathi", "Jhenaidah", "Joypurhat", "Khagrachhari", "Khulna", "Kishoreganj", "Kurigram", "Kushtia", "Lakshmipur", "Madaripur", "Magura", "Manikganj", "Meherpur", "Moulvibazar", "Munshiganj", "Mymensingh", "Naogaon", "Narail", "Narsingdi", "Natore", "Netrokona", "Nilphamari", "Noakhali", "Pabna", "Panchagarh", "Patuakhali", "Pirojpur", "Rajbari", "Rajshahi", "Rangamati", "Rangpur", "Satkhira", "Shariatpur", "Sherpur", "Sirajganj", "Sunamganj", "Sylhet", "Tangail", "Thakurgaon");
+  
+  @FXML
+  private TextField UserName;
+  @FXML
+  private TextField Name;
+  @FXML
+  private TextField Gender;
+  @FXML
+  private TextField Age;
+  @FXML
+  private TextField PhoneNum;
+  @FXML
+  private TextField Nid;
+  @FXML
+  private TextField Email;
+  @FXML
+  private TextField EmgContact;
+  @FXML
+  private TextField Blood;
+  @FXML
+  private TextField Allergy;
+  
+  private String Address;
+  
+          
+  
+  
+  
   
   public void LoginPageLink(ActionEvent event)throws IOException
   {
@@ -40,7 +69,37 @@ public class UserSignupController implements Initializable {
   
   public void initialize(URL location, ResourceBundle resources)
   {
-      combobox.setItems(list);
+      Addressbox.setItems(list);
+  }
+  
+  public void AddressInfo(ActionEvent event)
+  {
+      Address=Addressbox.getValue();
+  }
+  
+  public void CollectInfo()
+  {
+      
+      String userName= UserName.getText();
+      String name= Name.getText();
+      String gender= Gender.getText();
+      String age= Age.getText();
+      String phnNum= PhoneNum.getText();
+      String nid= Nid.getText();
+      String email= Email.getText();
+      String emgContact= EmgContact.getText();
+      String blood= Blood.getText();
+      String allergy= Allergy.getText();
+      
+      
+      UserSignupInfo usi= new UserSignupInfo(userName,name,gender,age,phnNum,nid,email,emgContact,Address,blood,allergy);
+      
+      usi.showInfo();
+      
+      
+     
+      
+      
   }
        
     
